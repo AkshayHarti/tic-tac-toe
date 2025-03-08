979dd71b885c4692591185e6c700a4898f48ccb6
Akshay Harti <akshay.r555@gmail.com>
2025-03-08 16:54:18 +1100

    chore: added dev docker compose

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.css'
 import { Board } from './components/Board'
 
 function App() {
-    return (
-        <header>
-            {/* 
+  return (
+    <header>
+      {/* 
         1. Create a cell component with a value and an onClick handler
         2. Create a board component with 9 cells
         3. Create a game component with a board and a cell component
       */}
-            <h1 className="text-3xl font-bold underline">Tic Tac Toe</h1>
-            <br /> <br />
-            <Board />
-        </header>
-    )
+      <h1 className='text-3xl font-bold underline'>Tick Tack Toe</h1>
+      <br /> <br />
+      <Board />
+    </header>
+  )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173,
+    },
+    watch: {
+      usePolling: true, // Enable polling (useful in Docker)
+      interval: 1000, // Check for changes every 1 second
+    },
+  },
 })
